d34cd2bb1a8297d1f9e40c7b2e7d4d4c677da1f4
Michael Ingraldi <protusmose@gmail.com>
2022-11-21 21:36:01 -0600

    Initial commit

---
+++ HelloName/HelloName/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloName.Program
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace HelloName
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Greetings, weary traveler. What is your name? ");
+                String name = Console.ReadLine();
+                Console.WriteLine("Welcome to NewHaven, " + name);
+            }
+        }
+    }
+}
---
+++ SimpleAddition/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SimpleAddition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            string input = Console.ReadLine();
+            int num1 = int.Parse(input);
+            Console.Write("Please enter your second number: ");
+            input = Console.ReadLine();
+            int num2 = int.Parse(input);
+            int sum = num1 + num2;
+            Console.WriteLine("The sum of your numbers is: " + sum);
+            Console.ReadLine();
+
+        }
+    }
+}
