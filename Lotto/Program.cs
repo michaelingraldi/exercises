0da56f33a46342f1201f34d02065a48739afc554
Michael Ingraldi <protusmose@gmail.com>
2023-09-29 08:02:04 -0500

    Finished preliminary input, array build, and display of results.
    To Do: Order the numbers, limiting by remaining set, evaluate results.

--- Lotto/Program.cs
+++ Lotto/Program.cs
@@ -1,22 +1,46 @@
 ﻿Random random = new Random();
 
-//Sets up Array
+
+//Sets up Arrays
+//Player Guesses
 
 int arraySize = 5;
-int[] randomArray = new int[arraySize];
+int[] guessArray = new int[arraySize];
+
+//Results
+int[] resultsArray = new int[arraySize];
+
+//Collect player input
+Console.WriteLine("Let's Play The Lottery!");
+Console.WriteLine("Time to pick your numbers.");
+
+
+for (int i = 0; i < guessArray.Length; i++)
+{
+    Console.Write("Enter a number between 1 and 70, then press Enter");
+    string input = Console.ReadLine();
+    int parsedInput = int.Parse(input); // Parse the input and store it in a variable
+    guessArray[i] = parsedInput; // Store the parsed integer in the guessArray
+}
+
 
 //Populates the array of numbers
 
 for (int i = 0; i < arraySize; i++)
 {
     int lowerLimit = i + 1;
-    randomArray[i] = random.Next(lowerLimit,70);
+    resultsArray[i] = random.Next(lowerLimit,70);
 }
 
 //Displays the results
-
-Console.WriteLine("Winning Numbers:");
-foreach (int value in randomArray)
+Console.WriteLine("Your Numbers: ");
+foreach (int value in guessArray)
 {
     Console.WriteLine(value);
 }
+Console.WriteLine("Winning Numbers:");
+foreach (int value in resultsArray)
+{
+    Console.WriteLine(value);
+}
+Console.ReadLine();
